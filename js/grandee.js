c64f46b61314350e39508308164ce32f0f7324ff
Dan O'Huiginn <daniel@ohuiginn.net>
2014-09-03 19:07:39 +0200

    Stub out UI framework + loading mechanism

--- js/grandee.js
+++ js/grandee.js
@@ -1,14 +1,32 @@
 /*jslint vars: true */
 
-
 var grandee = function () {
     "use strict";
-
-    var transforms = [];
-    var visuals = [];
-
-    var initialize = function () {
-    // call this once all modules are loaded
-    };
     
-};
+    var ui = undefined;
+    var transforms = {};
+    var ui_available = {};
+
+    return {
+	// these need to be public so plugins can add themselves here
+	transforms: transforms,
+	ui_available: ui_available,
+
+	setVisualization: function (uiname) {
+          // set the visualization, by name
+	  if(ui_available.hasOwnProperty(uiname)){
+	      this.ui = ui_available[uiname];
+	      console.log('set UI to ' + uiname);
+	      return true;
+	      }
+	  else{
+	      console.log('unable to set UI to ' + uiname);
+	      return false;
+	      }
+	  },
+	initialize: function () {
+ 	    console.log('initializing grandee');
+	    // call this once all modules are loaded
+	},
+	}
+}();
---
+++ ui/basic.js
@@ -0,0 +1,10 @@
+
+grandee.ui_available.basic = (function(){
+    return {
+	name: 'Basic User Interface',
+	
+	draw: function(network_map){
+	    console.log('drawing');
+	    }
+	}
+    })();
