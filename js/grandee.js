a4184ba37a98dd77e945c668d8a494202e73d63b
Dan O'Huiginn <daniel@ohuiginn.net>
2014-09-03 17:10:28 +0200

    Start stubbing out the core JS

---
+++ js/grandee.js
@@ -0,0 +1,14 @@
+/*jslint vars: true */
+
+
+var grandee = function () {
+    "use strict";
+
+    var transforms = [];
+    var visuals = [];
+
+    var initialize = function () {
+    // call this once all modules are loaded
+    };
+    
+};
